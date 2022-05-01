4a195eaddfe73e3b0515811989851d8d1a514ce9
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2022-05-01 15:02:35 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Search from "./Components/Search";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NavBar } from "./Components/NavBar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ import Home from "./Components/Home";
+// import ItemInfo from "./Components/ItemInfo";
+// import ItemAdd from "./Components/ItemAdd";
+// import ItemEdit from "./Components/ItemEdit";
+// import AddModal from "./Components/AddModal";
+// import GetAllData from "./Components/GetAllData";
+
+
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+  <NavBar/>
+   <Routes>
+  <Route path="/Home" element={<Home />} />
+   </Routes>
     <App />
-  </React.StrictMode>
+    
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
