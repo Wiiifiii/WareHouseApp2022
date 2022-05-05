e48242fdfe239770877556c08e64418f8907963a
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2022-05-06 02:18:47 +0300

    fix add item component

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -2,7 +2,7 @@ import { Form, Col, Row, Button, Modal, Alert } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import { VscCheck } from "react-icons/vsc";
 import Inventory from "./Inventory";
-
+import { Link } from "react-router-dom";
 function AddItem() {
   const [show, setShow] = useState(true);
 
@@ -15,7 +15,7 @@ function AddItem() {
   const [productnumber, setProductNumber] = useState("");
   const [shelfid, setShelfId] = useState("");
   const [action, setAction] = useState("");
-  const [quantity, setQuantity] = useState(new Number ());
+  const [quantity, setQuantity] = useState();
   const [img, setImg] = useState("");
   const [actionDate, setActionDate] = useState(new Date());
   const [input, setInput] = useState("");
@@ -110,12 +110,69 @@ function AddItem() {
             <Row>
               <Col>
                 {" "}
-                <Form.Control
-                  type="text"
-                  placeholder="Shelf code"
-                  value={shelfid}
-                  onChange={(e) => setShelfId(e.target.value)}
-                />
+                <Form.Select
+              
+              value={shelfid}
+              onChange={(e) => setShelfId(e.target.value)}
+            >
+               <option>Shelf Code...</option>
+               <option>A-01</option>
+              <option>A-02</option>
+              <option>B-01</option>
+              <option>B-02</option>
+              <option>C-01</option>
+              <option>C-02</option>
+              <option>D-01</option>
+              <option>D-02</option>
+              <option>C-01</option>
+              <option>C-02</option>
+              <option>D-01</option>
+              <option>D-02</option>
+              <option>E-01</option>
+              <option>E-02</option>
+              <option>F-01</option>
+              <option>F-02</option>
+              <option>G-01</option>
+              <option>G-02</option>
+              <option>H-01</option>
+              <option>H-02</option>
+              <option>I-01</option>
+              <option>I-02</option>
+              <option>J-01</option>
+              <option>J-02</option>
+              <option>K-01</option>
+              <option>K-02</option>
+              <option>L-01</option>
+              <option>L-02</option>
+              <option>M-01</option>
+              <option>M-02</option>
+              <option>N-01</option>
+              <option>N-02</option>
+              <option>O-01</option>
+              <option>O-02</option>
+              <option>P-01</option>
+              <option>P-02</option>
+              <option>Q-01</option>
+              <option>Q-02</option>
+              <option>R-01</option>
+              <option>R-02</option>
+              <option>S-01</option>
+              <option>S-02</option>
+              <option>T-01</option>
+              <option>T-02</option>
+              <option>U-01</option>
+              <option>U-02</option>
+              <option>V-01</option>
+              <option>V-02</option>
+              <option>W-01</option>
+              <option>W-02</option>
+              <option>X-01</option>
+              <option>X-02</option>
+              <option>Y-01</option>
+              <option>Y-02</option>
+              <option>Z-01</option>
+              <option>Z-02</option>
+             </Form.Select>
               </Col>
             </Row>
             <Row>
@@ -161,7 +218,7 @@ function AddItem() {
                 {" "}
                 <Form.Control
                   type="date"
-                  
+                  hidden
                  disabled
                   value={actionDate}
                   onChange={(e) => setActionDate(e.target.value)}
@@ -193,6 +250,9 @@ function AddItem() {
           </Form>
         </Modal.Body>
         <Modal.Footer>
+        <Link to={`/Inventory/`}>
+              <Button variant="outline-secondary">Go to Inventory</Button>
+            </Link>
           <Button
             variant="outline-secondary"
             onClick={() => {
@@ -230,6 +290,18 @@ function AddItem() {
           )}
         </Modal.Footer>
       </Modal>
+      <div style={{padding: '20px'}}> 
+      <Link to={`/AddItem/`}>
+      <Button 
+              variant="outline-secondary"
+              onClick={() => {
+                refreshPage();
+                changeBtnState();
+                setMessage(false);
+              }}
+            >Add Product</Button>
+            </Link>
+            </div>
       <div>
         <Inventory />
       </div>
